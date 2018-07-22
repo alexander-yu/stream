d9d70117e7487f99dc3ce56f86e7adcc56e935ce
Alex Yu <yu.alex96@gmail.com>
2018-07-22 15:04:53 -0400

    Add reservoir sampling

---
+++ sample.go
@@ -0,0 +1,29 @@
+package stream
+
+import (
+	"math/rand"
+)
+
+// Reservoir is a struct for performing reservoir sampling on a stream.
+type Reservoir struct {
+	Size   int
+	sample []interface{}
+	count  int
+}
+
+// Push consumes a value to perform reservoir sampling.
+func (r *Reservoir) Push(x interface{}) {
+	r.count++
+	if r.count <= r.Size {
+		r.sample = append(r.sample, x)
+	} else if index := rand.Intn(r.count); index < r.Size {
+		r.sample[index] = x
+	}
+}
+
+// Sample returns a copied slice of the obtained sample.
+func (r *Reservoir) Sample() []interface{} {
+	sample := make([]interface{}, len(r.sample))
+	copy(sample, r.sample)
+	return sample
+}
---
+++ sample_test.go
@@ -0,0 +1,35 @@
+package stream
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func testReservoir() *Reservoir {
+	r := &Reservoir{Size: 3}
+	rand.Seed(1)
+
+	for i := 0; i < 10; i++ {
+		r.Push(i)
+	}
+
+	return r
+}
+
+func TestReservoirPush(t *testing.T) {
+	r := testReservoir()
+	assert.Equal(t, []interface{}{6, 7, 4}, r.sample)
+}
+
+func TestSample(t *testing.T) {
+	r := testReservoir()
+	sample := r.Sample()
+
+	assert.Equal(t, []interface{}{6, 7, 4}, sample)
+
+	sample[0] = 1
+
+	assert.Equal(t, []interface{}{6, 7, 4}, r.sample)
+}
