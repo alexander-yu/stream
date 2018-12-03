a1055b13d308015398da5e9124a7d534864fb297
Alex Yu <yu.alex96@gmail.com>
2018-12-03 01:17:13 -0500

    add lock to Reservoir

--- sample/sample.go
+++ sample/sample.go
@@ -2,6 +2,7 @@ package sample
 
 import (
 	"math/rand"
+	"sync"
 )
 
 // Reservoir is a struct for performing reservoir sampling on a stream.
@@ -9,10 +10,14 @@ type Reservoir struct {
 	Size   int
 	sample []interface{}
 	count  int
+	mux    sync.Mutex
 }
 
 // Push consumes a value to perform reservoir sampling.
 func (r *Reservoir) Push(x interface{}) {
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
 	r.count++
 	if r.count <= r.Size {
 		r.sample = append(r.sample, x)
@@ -23,6 +28,9 @@ func (r *Reservoir) Push(x interface{}) {
 
 // Sample returns a copied slice of the obtained sample.
 func (r *Reservoir) Sample() []interface{} {
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
 	sample := make([]interface{}, len(r.sample))
 	copy(sample, r.sample)
 	return sample
