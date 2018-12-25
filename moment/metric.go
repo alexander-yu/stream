d37b18f0dc066c1ca784f6bbbd38a33af36aa72e
Alex Yu <yu.alex96@gmail.com>
2018-12-25 00:08:00 -0500

    [moment] Rename moment_metric to metric (avoid stuttering)

--- moment/metric.go
+++ moment/metric.go
@@ -2,7 +2,7 @@ package moment
 
 import "github.com/alexander-yu/stream"
 
-// Metric is the interface for a metric tracks the moment of a stream.
+// Metric is the interface for a metric that tracks the moment of a stream.
 type Metric interface {
 	stream.Metric
 	Subscribe(*Core)
